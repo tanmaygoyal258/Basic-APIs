8a772abebfe625c2bb7fba911d059a567663b114
Tanmay Goyal <gotanmay@gmail.com>
2022-01-02 22:58:42 +0530

    Used nodemon to directly reflect changes

--- express-demo/index.js
+++ express-demo/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app  = express();           // creates object app with methods .get() , .post() , .delete() , .put()
 
 app.get('/' , (req,res)=>{
-    res.send("hello World");
+    // res.send("hello World");
+    res.send("hello World, this is a change that nodemon should pick up");
 });
 
 app.get('/api/courses' , (req,res)=>{
