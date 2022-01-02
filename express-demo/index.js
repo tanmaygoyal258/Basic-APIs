ab72dc42223535ff5607fd96f826d698388bb008
Tanmay Goyal <gotanmay@gmail.com>
2022-01-02 23:45:42 +0530

    dealt with parametres and query parametres

--- express-demo/index.js
+++ express-demo/index.js
@@ -10,7 +10,18 @@ app.get('/api/courses' , (req,res)=>{
     res.send([1,2,3]);
 });
 
+app.get('/api/courses/:id' , (req,res)=>{
+    res.send(req.params.id);                            // prints the id parametre
+});
+
+app.get('/api/courses/:year/:month' , (req,res)=>{
+    //res.send([req.params.month , req.params.year]);           // dealing with multiple parametres
+    res.send(req.query);                                        // dealing with query, inserted after a question mark
+})
+
+
+
 const port = process.env.PORT || 3000 ;     // if we have an environment variable port, take it else default = 3000
 // app.listen(port , ()=> console.log("Listening on Port 3000"));
 
-app.listen(port , ()=> console.log(`Listening on port ${port}`));       //dynamic port
+app.listen(port , ()=> console.log(`Listening on port ${port}`));       // dynamic port
